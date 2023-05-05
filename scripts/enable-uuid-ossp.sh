5860ad098cd0947ddca03e176c6e6e36eca3c692
Vyom-Yadav <jackhammervyom@gmail.com>
2023-05-05 23:56:13 +0530

    fix: Correct extension name in echo
    Signed-off-by: Vyom-Yadav <jackhammervyom@gmail.com>

--- scripts/enable-uuid-ossp.sh
+++ scripts/enable-uuid-ossp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "enabling pg_trgm on database $POSTGRES_DB"
+echo "enabling uuid-ossp on database $POSTGRES_DB"
 psql -U "$POSTGRES_USER" --dbname="$POSTGRES_DB" <<-'EOSQL'
   CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 EOSQL
